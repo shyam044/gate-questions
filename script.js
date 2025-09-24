adb33c0d4dae691132c3b4cf3836aa49b5c5604f
shyam044 <mogantimindhivarashyamsai@gmail.com>
2025-09-24 17:33:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,65 +1,32 @@
-let topic=document.getElementById("topics");
-let algo_topic = document.getElementById("algorithmtopics");
-let container = document.getElementById("algo-container");
+// Toggle function for a section
+function toggleSection(buttonId, sectionId) {
+  const button = document.getElementById(buttonId);
+  const section = document.getElementById(sectionId);
 
-// Initially hide the topics
-algo_topic.classList.add("d-none");
-
-// Show on hover of entire container (button + topics)
-container.addEventListener("mouseenter", function () {
-    algo_topic.classList.remove("d-none");
-});
-
-// Hide when mouse leaves the container
-container.addEventListener("mouseleave", function () {
-    algo_topic.classList.add("d-none");
-    greedy_topic.classList.add("d-none");
-    dynamic_topic.classList.add("d-none");
-});
-
-let greedy_approach=document.getElementById("greedyapproach");
-let greedy_topic = document.getElementById("greedytopic");
-greedy_topic.classList.add("d-none");
-
-greedy_approach.onclick=function()
-{
-    greedy_topic.classList.remove("d-none");
+  button.addEventListener('click', () => {
+    section.classList.toggle('d-none');
+  });
 }
 
-let dynamic_programming=document.getElementById("dynamicprogramming");
-let dynamic_topic = document.getElementById("dynamictopic");
-dynamic_topic.classList.add("d-none");
+// Show/hide nested topics
+function toggleNested(buttonId, topicId) {
+  const button = document.getElementById(buttonId);
+  const topic = document.getElementById(topicId);
 
-dynamic_programming.onclick=function()
-{
-    dynamic_topic.classList.remove("d-none");
+  button.addEventListener('click', () => {
+    topic.classList.toggle('d-none');
+  });
 }
 
+// Apply to all topics
+toggleSection('clickalgo', 'algorithmtopics');
+toggleSection('clickds', 'DStopics');
+toggleSection('clickdbms', 'dbmstopics');
+toggleSection('clickcn', 'cntopics');
+toggleSection('clickcompiler', 'compilertopics');
 
-let ds_topic=document.getElementById("DStopics");
-let click2=document.getElementById("clickds");
-ds_topic.classList.add("d-none");
-click2.onclick=function()
-{
-    ds_topic.classList.remove("d-none");
-}
+toggleNested('greedyapproach', 'greedytopic');
+toggleNested('dynamicprogramming', 'dynamictopic');
 
-
-
-let dbms_topic = document.getElementById("dbmstopics");
-let dbms_container = document.getElementById("dbms-container");
-
-// Initially hide the topics
-dbms_topic.classList.add("d-none");
-
-// Show on hover of entire container (button + topics)
-dbms_container.addEventListener("mouseenter", function () {
-    dbms_topic.classList.remove("d-none");
-});
-
-// Hide when mouse leaves the container
-dbms_container.addEventListener("mouseleave", function () {
-    dbms_topic.classList.add("d-none");
-});
 
 
